e89baf6b7eec8d2af6cee600408ba397d9b3f22d
Ryan <rstachurr@gmail.com>
2024-02-23 22:16:59 -0500

    i have nothing valuable to say

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from time       import sleep
+from time       import time, sleep
 from signal     import signal, SIGINT
 from threading  import Thread, currentThread
 from builtins   import print as builtin_print
@@ -9,7 +9,6 @@ g_client                  = None
 g_connection_established  = False
 g_was_signal_caught       = False
 g_thread_print_names      = dict()
-
 
 # Override to prefix user-defined thread name
 def print(*args, **kwargs):
@@ -23,13 +22,7 @@ def print(*args, **kwargs):
 
     builtin_print(*args, **kwargs)
 
-def signal_handler(signum, _frame):
-    global g_server, g_was_signal_caught
-
-    g_was_signal_caught = True
-    print('-=-=- Caught signal %d -=-=-' % signum)
-    return
-
+# Set/clear the calling thread's print name.
 def set_thread_print_name(name: str = None):
     global g_thread_print_names
 
@@ -39,6 +32,14 @@ def set_thread_print_name(name: str = None):
         g_thread_print_names.pop(id, None)
     else:
         g_thread_print_names[id] = f'[{name}]'
+
+# Catch interrupts.
+def signal_handler(signum, _frame):
+    global g_server, g_was_signal_caught
+
+    g_was_signal_caught = True
+    print('-=-=- Caught signal %d -=-=-' % signum)
+    return
 
 
 
@@ -60,7 +61,7 @@ def on_accept_callback(did_succeed: bool):
     if did_succeed:
         print('Connected to %s:%d' % g_client.peer)
 
-def async_accept(ip: str, port: int, callback):
+def _async_accept(ip: str, port: int, callback):
     global g_server, g_client, g_connection_established
 
     set_thread_print_name('TCP Server')
@@ -77,62 +78,61 @@ def async_accept(ip: str, port: int, callback):
             break
         status = True
     except Exception as e:
-        print('ERROR: Failed to make connection:\n\t', e)
+        print('ERROR:', e)
     
     # Hit callback.
     callback(status)
     return
 
-def listener(ip: str, port: int):
-    global g_server, g_client, g_connection_established, g_was_signal_caught
+def _advanced_interact():
+    global g_client, g_server
 
-    # Start TCP server on a new thread.
-    t = Thread(target=async_accept, args=(ip, port, on_accept_callback))
-    t.start()
-    
-    # Wait for a connection or ctrl+c interrupt.
-    while not g_connection_established:
-        if g_was_signal_caught:
-            print('Interrupted before a connection request was received.')
+    command = ''
+    while command != 'exit':
+        # Exit the loop if the server was closed.
+        if g_server is None or g_server.closed:
+            print('Server closed unexpectedly.')
             break
 
-        # No need to belligerent.
+        try:
+            # get output until dollar sign (bash --posix forces bash-X.X$)
+            recv_bytes = g_client.read_until('$')
+            print(recv_bytes.decode('utf-8'), end='') # print string of received bytes
+
+            # get user input command and write command to socket
+            command = input(' ')
+            g_client.write(command + '\n')
+
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt')
+            break
+        
+        except Exception as e:
+            print('Unknown error:', e)
+            break
+
+def listener(ip: str, port: int, advanced_interact: bool = False):
+    global g_client, g_connection_established, g_was_signal_caught
+
+    # Start TCP server on a new thread.
+    t = Thread(target=_async_accept, args=(ip, port, on_accept_callback))
+    t.start()
+    
+    # Busy wait for a connection or ctrl+c interrupt.
+    while not g_connection_established and not g_was_signal_caught:
         sleep(0.2)
 
     # Are we connected?
     if g_connection_established:
-        command = ''
-        while command != 'exit':
-            # Exit the loop if the g_server was closed.
-            if g_server is None or g_server.closed:
-                print('Server closed unexpectedly.')
-                break
-            
-            # uwu
-            try:
-                # get output until dollar sign (bash --posix forces bash-X.X$)
-                data = g_client.read_until('$')
-                # data = g_client.read_until('#')
-                # data = g_client.read(4096, 1.0)
-                # data = g_client.interact()
-                print(data.decode('utf-8'), end='')  # print string of received bytes
-    
-                # get user input command and write command to socket
-                # command = input('victim-machine> ')
-                command = input(' ')
-                g_client.writeln(command)
-    
-            except KeyboardInterrupt:
-                print('Keyboard Interrupt')
-                break
-            
-            except Exception as e:
-                print('Unknown error:', e)
-                break
-
+        if advanced_interact:
+            _advanced_interact()
+        else:
+            g_client.interact()
         print('Disconnected.')
+    else:
+        print('Canceled before a connection request was received.')
 
-    # Make sure g_server is dead so we can wrangle it's corpse.
+    # Make sure server is dead so we can wrangle it's corpse.
     close_server()
     t.join()
     return
@@ -140,9 +140,10 @@ def listener(ip: str, port: int):
 
 
 
-import socket
-import select
-import struct
+from socket import socket, socketpair, SOL_TCP, TCP_INFO, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from select import select
+from struct import unpack
+from nclib  import simplesock
 
 def getTCPInfo(s):
     '''struct tcp_info
@@ -183,57 +184,86 @@ def getTCPInfo(s):
         u_int32_t	tcpi_reordering;
     };'''
 
-    x = struct.unpack('B'*7+'I'*21, s.getsockopt(socket.SOL_TCP, socket.TCP_INFO, 1*7+4*21))
+    x = unpack('B'*7+'I'*21, s.getsockopt(SOL_TCP, TCP_INFO, 1*7+4*21))
     print(x)
 
 def netcat(hostname, port, content):
     host = (hostname, port)
 
     print('Creating socket')
-    sock = socket.socket(type=socket.SOCK_STREAM)
+    sock = socket(type=SOCK_STREAM)
     print('Setting options')
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     print('Binding')
     sock.bind(host)
     print('Listening')
     sock.listen()
 
-    # print('r/w pair')
-    # _rsock, _wsock = socket.socketpair()
-    #
-    # print('select')
-    # rl, _, _ = select.select([sock, _rsock], [], [])
-    # if _rsock in rl:
-    #     print('closing r/w')
-    #     _rsock.close()
-    #     _wsock.close()
+    print('r/w pair')
+    _rsock, _wsock = socketpair()
+    print('select')
+    rl, _, _ = select([sock, _rsock], [], [])
+    if _rsock in rl:
+        print('closing r/w')
+        _rsock.close()
+        _wsock.close()
 
     print('Waiting for connection...')
     client, addr = sock.accept()
     # yield Netcat(sock=client, server=addr)
     print('Connected to %s:%d' % addr)
     
-    print('  - blocking =', client.getblocking())
-    print('  - timeout  =', client.gettimeout())
-
-    # print('Setting blocking to False')
-    # client.setblocking(False)
     # print('  - blocking =', client.getblocking())
     # print('  - timeout  =', client.gettimeout())
 
-    client.sendall(b'ls -l')
+    print('Setting blocking to False')
+    client.setblocking(False)
+    # print('  - blocking =', client.getblocking())
+    # print('  - timeout  =', client.gettimeout())
 
+    # client.sendall(b'ls -l')
+
+    client = simplesock.wrap(client)
+    timeout = None
 
     data = b''
     while not g_was_signal_caught:
+        print('signal still not caught')
+        deadline = None
+        if timeout is not None:
+            deadline = time() + timeout
+
         try:
-            recv = client.recv(1)
+            first_shot = True
+            while True:
+                print('still looping')
+                if deadline is not None:
+                    timeout = deadline - time()
+                    if timeout < 0:
+                        if first_shot:
+                            timeout = 0
+                        else:
+                            raise 'TIMED OUT'
+                first_shot = False
+
+                if timeout is not None:
+                    print('waiting for timeout')
+                    r, _, _ = select([client], timeout=timeout)
+                    if not r:
+                        raise 'TIMED OUT (select)'
+                
+                try:
+                    recv = client.recv(1)
+                except ConnectionResetError:
+                    recv = b''
+                
+                if not recv:
+                    break
+
+                data += recv
+
         except Exception as e:
             print('Exception:', e)
-            break
-
-        if recv == b'':
-            print('Received all data')
             break
             
     print('data =', data)
@@ -251,10 +281,7 @@ if __name__ == '__main__':
     print('-=-=- Start -=-=-')
     
     set_thread_print_name()
-    set_thread_print_name()
-    set_thread_print_name()
-    set_thread_print_name()
-    listener('0.0.0.0', 6969)
+    listener('0.0.0.0', 6969, advanced_interact=True)
     # netcat('0.0.0.0', 6969, None)
     set_thread_print_name('Main')
     
